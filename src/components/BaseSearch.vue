a24c862c881b4bb7724a66eb7f77cbcbf59b0bcd
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2020-12-22 21:05:57 +0630

    nav bar raw

---
+++ src/components/BaseSearch.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-text-field
+    @focus="searchClosed = false"
+    @blur="searchClosed = true"
+    prepend-inner-icon="mdi-magnify"
+    placeholder="Search"
+    class="expanding-search"
+    :class="{ closed: searchClosed }"
+    dense
+    outlined
+    background-color="#F5F5F5"
+    elevation="0"
+    :clearable="!searchClosed"
+    color="#5056D2"
+  >
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  name: "TheNav",
+  data: () => ({
+    drawer: false,
+    group: null,
+    searchClosed: true,
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.v-input.expanding-search
+   max-height: 45px
+.closed
+   max-width: 45px
+</style>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <v-app-bar app color="white" flat fixed class="nav-bar">
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="d-flex d-md-none"
+      ></v-app-bar-nav-icon>
+
+      <v-img
+        class="d-none d-md-flex"
+        src="/img/logo.png"
+        max-width="80px"
+      ></v-img>
+      <v-toolbar-title class="pl-3 pl-md-0">Mtuvoter</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <div>
+        <BaseSearch />
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary fixed app>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text darken-2 text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+import BaseSearch from "@/components/BaseSearch.vue";
+export default {
+  name: "TheNav",
+  components: {
+    BaseSearch,
+  },
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped></style>
